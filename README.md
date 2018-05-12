456dfc4e9809032708539f6b712856e82b248578
Kenta, Kobayashi <kentafly88@gmail.com>
2018-05-12 17:07:16 +0900

    minil new

---
+++ README.md
@@ -0,0 +1,22 @@
+# NAME
+
+Lodash - It's new $module
+
+# SYNOPSIS
+
+    use Lodash;
+
+# DESCRIPTION
+
+Lodash is ...
+
+# LICENSE
+
+Copyright (C) Kenta, Kobayashi.
+
+This library is free software; you can redistribute it and/or modify
+it under the same terms as Perl itself.
+
+# AUTHOR
+
+Kenta, Kobayashi <kentafly88@gmail.com>
---
+++ lib/Lodash.pm
@@ -0,0 +1,39 @@
+package Lodash;
+use 5.008001;
+use strict;
+use warnings;
+
+our $VERSION = "0.01";
+
+
+
+1;
+__END__
+
+=encoding utf-8
+
+=head1 NAME
+
+Lodash - It's new $module
+
+=head1 SYNOPSIS
+
+    use Lodash;
+
+=head1 DESCRIPTION
+
+Lodash is ...
+
+=head1 LICENSE
+
+Copyright (C) Kenta, Kobayashi.
+
+This library is free software; you can redistribute it and/or modify
+it under the same terms as Perl itself.
+
+=head1 AUTHOR
+
+Kenta, Kobayashi E<lt>kentafly88@gmail.comE<gt>
+
+=cut
+
